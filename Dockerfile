e63c64e3aad0e45b37fca454b555352f37cf2ca8
maany <imptodefeat@gmail.com>
2019-08-05 16:26:32 +0200

    dockerize the shizz

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3
+ENV PYTHONUNBUFFERED 1
+RUN apt-get update && apt-get -y install cron vim
+RUN mkdir /code
+WORKDIR /code
+ADD requirements.txt /code/
+RUN pip install -r requirements.txt
+ADD . /code/
+ENTRYPOINT /code/init.sh
---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+### CRON ####
+export PYTHONPATH=/code:${PYTHONPATH}
+export DJANGO_SETTINGS_MODULE='rucio_opint_backend.apps.core.settings'
+python manage.py makemigrations core
+python manage.py migrate
+python manage.py loaddata initial
+echo "0 * * * * root /usr/bin/python3 /code/manage.py rucio_loader_cron >> /code/rucio_loader_output" >> /etc/crontab
+service cron start
+python3 manage.py runserver 0.0.0.0:8000
+
+
+
