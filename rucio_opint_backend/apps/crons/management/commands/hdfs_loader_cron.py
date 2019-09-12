237f09204b723ad6d1757664cb124b4ea006c07e
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-09-12 15:07:23 +0200

    adds hdfs loader

--- rucio_opint_backend/apps/crons/management/commands/hdfs_loader_cron.py
+++ rucio_opint_backend/apps/crons/management/commands/hdfs_loader_cron.py
@@ -5,17 +5,13 @@ from pyspark.sql import SparkSession
 
 from django.core.management.base import BaseCommand
 
+from rucio_opint_backend.apps.utils.tools import parse_date
+
 
 class Command(BaseCommand):
     help = 'Runs the HDFS fetching job'
 
-    def parse_date(self, date):
-        for fmt in ('%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y'):
-            try:
-                return datetime.datetime.strptime(date, fmt)
-            except ValueError:
-                pass
-        raise ValueError('no valid date format found')
+    base_path = 'project/monitoring/archive/rucio/raw/events'
 
     def add_arguments(self, parser):
         parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='File with files to be imported')
@@ -26,18 +22,37 @@ class Command(BaseCommand):
         print("Importing HDFS Data")
         self.populate(**options)
 
+    def construct_path_for_date(self, date):
+        return self.base_path + F'/{date.year}/{date.month:02d}/{date.day:02d}/'
+
+    def pull_hdfs_dir(self, path, spark):
+        try:
+            fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
+            list_status = fs.listStatus(spark._jvm.org.apache.hadoop.fs.Path(path))
+            for file in [file.getPath().getName() for file in list_status]:
+                self.pull_hdfs_json(path+file, spark)
+        except Exception as e:
+            print('Error listing files for ', path, e)
+
+
+    def pull_hdfs_json(self, path, spark):
+        try:
+            res = spark.read.json(path)
+            print(res)
+        except Exception as e:
+            print('Error loading data from', path, e)
+
     def populate(self, **options):
         spark = SparkSession.builder.master("local[*]").appName("Issues").getOrCreate()
-        print(spark)
         if options.get('date'):
-            date = self.parse_date(options['date'])
-            print('Will read data for', date)
+            path = self.construct_path_for_date(parse_date(options['date']))
+            self.pull_hdfs_dir(path, spark)
         if options.get('range'):
             today = datetime.datetime.today()
             date_list = [today - datetime.timedelta(days=x) for x in range(options['range'])]
-            print('will read data for ')
             for date in date_list:
-                print(date)
+                path = self.construct_path_for_date(date)
+                self.pull_hdfs_dir(path, spark)
         if options.get('file'):
             for line in options.get('file'):
-                print('wIll rad data from', line)
+                self.pull_hdfs_json(line, spark)
---
+++ rucio_opint_backend/apps/utils/tools.py
@@ -0,0 +1,9 @@
+import datetime
+
+def parse_date(date):
+    for fmt in ('%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y'):
+        try:
+            return datetime.datetime.strptime(date, fmt)
+        except ValueError:
+            pass
+    raise ValueError('no valid date format found')
