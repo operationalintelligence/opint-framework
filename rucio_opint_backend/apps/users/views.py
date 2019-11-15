2f210c3f837494a6c7520b197e43be8e0d206529
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-11-15 13:46:19 +0100

    final oauth fixes

--- rucio_opint_backend/apps/users/views.py
+++ rucio_opint_backend/apps/users/views.py
@@ -1,22 +1,10 @@
-import traceback
-
 from rest_auth.registration.views import SocialLoginView
 
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client, OAuth2Error
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.providers.cern.views import CernOAuth2Adapter
-
-
-class HandledOAuth2Client(OAuth2Client):
-
-    def get_access_token(self, code):
-        try:
-            super().get_access_token(code)
-        except OAuth2Error:
-            print(traceback.format_exc())
-            return {'error': 'OAuth2Client Error'}
 
 
 class CERNLogin(SocialLoginView):
     adapter_class = CernOAuth2Adapter
     callback_url = 'https://rucio-opint-ui-dev.web.cern.ch/login/cern/success'
-    client_class = HandledOAuth2Client
+    client_class = OAuth2Client
