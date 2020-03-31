da87194a65eadc06a20f4e59f4edb902d897439d
Luca Clissa <luca.clissa2@unibo.it>
2020-03-31 20:11:36 +0200

    Add vectorization step to run function in luca_nlp_adapter.py

--- opint_framework/apps/example_app/nlp/luca/luca_nlp_adapter.py
+++ opint_framework/apps/example_app/nlp/luca/luca_nlp_adapter.py
@@ -39,7 +39,29 @@ class LucaNLPAdapter(NLPAdapter):
         self.context['dataset'] = test_errors
 
     def run(self):
-        vector_data = self.tokenization.tokenize_messages()
+        # LUCA: consider the following:
+        # 1) add base_w2v_path and data_window as arguments;
+        # 2) add w2v hyperparams as arguments;
+        # 3) add w2v_mode argument ("train" or "load") for training/inference modes
+        # 4) add kmeans_mode argument ("train" or "load", currently just "train" available)
+
+        token_data = self.tokenization.tokenize_messages()
+
+        # setup w2v model paths
+        base_w2v_path = "results/sample_app/" # NOTE: this is a Hadoop path
+        data_window = "9-13mar2020"
+        emb_size = 150
+        min_count = 500
+        win_size = 8
+        w2v_path = "{}/w2v_models/data_window_{}/w2v_VS={}_MC={}_WS={}".format(base_w2v_path, data_window, emb_size, min_count, win_size)
+
+        if False: # substitute with: w2v_mode=="train":
+            vector_data = self.vectorization.train_model(token_data, path_to_model=w2v_path, tks_col="stop_token_1",
+                                                         id_col=id_col, out_col='message_vector', embedding_size=emb_size,
+                                                         window=win_size, min_count=min_count, workers=12, mode="new")
+        elif True: # substitute with: w2v_mode=="load":
+            w2v_model = self.vectorization.load_model(w2v_path)
+            vector_data = w2v_model.transform(tks_data)
 
 
     def post_process(self):
