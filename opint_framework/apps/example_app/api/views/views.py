cd226389dfd2b99fbe7507c061ec5f62290fe771
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2020-04-14 16:11:12 +0200

    updates example app api

--- opint_framework/apps/example_app/api/views/views.py
+++ opint_framework/apps/example_app/api/views/views.py
@@ -1,10 +1,8 @@
-from rest_framework import viewsets
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
-class SampleViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint example
-    """
-    def index(request):
-        return Response({"Result":"OK"})
-
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def index(request):
+    return Response({"Result": "OK"})
--- opint_framework/apps/example_app/urls.py
+++ opint_framework/apps/example_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .api.views import SampleViewSet
+from .api.views.views import index
 
 urlpatterns = [
-
     # Could be accessed as http://127.0.0.1:8000/example_app/api/
-    path("", SampleViewSet.index, name='index'),
+    path("", index, name='index'),
 ]
