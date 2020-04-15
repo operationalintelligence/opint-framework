30d2d64744b3c591b89c81973b1fa37628281a6b
Boris Vasilev <borisvasilev395@gmail.com>
2020-04-15 13:48:38 +0100

    Add operators console models

--- opint_framework/apps/operators_console/models/models.py
+++ opint_framework/apps/operators_console/models/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 
 
-class SampleModel(models.Model):
+class PredictionHistory(models.Model):
     """
-    Sample Model.
+    Workflow Prediction History model.
     """
-    sample_message = models.CharField(max_length=1024)
+    hid = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    good = models.FloatField()
+    acdc = models.FloatField()
+    resubmit = models.FloatField()
+    timestamp = models.DateTimeField()
+
+
+class LabelArchive(models.Model):
+    """
+    Labels Archive model.
+    """
+    name = models.CharField(max_length=255, unique=True, primary_key=True)
+    label = models.IntegerField()
+
+
+class DocsOneMonthArchive(models.Model):
+    """
+    Short-term Document Archive model.
+    """
+    name = models.CharField(max_length=255)
+    document = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
